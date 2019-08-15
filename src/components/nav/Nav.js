f5ba7300bc1e82b980e65edf621e1c7877d5fe34
szx9231 <szx9231@gmail.com>
2019-08-15 17:05:37 +0800

    add mobile style

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './Nav.css';
 
 class Nav extends React.Component {
   render() {
     return (
       <nav className="main-navigation">
+        
         <div className="container">
           <div className="row">
             <div className="col-sm-12">
               <div className="navbar-header">
-                <span>
-                <i className="fa fa-bars"></i>
-                </span>
+                <FontAwesomeIcon className="hamburger-btn" size='2x' icon={faBars} />
+                <a className="slogan" href="/">多看多想多写多思考</a> 
               </div>
               <div className="navbar-collapse navbar" id="main-menu">
                 <ul className="menu">
