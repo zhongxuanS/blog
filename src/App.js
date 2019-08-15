968764448d1e295977f2e17836a9d93e25ee98ab
szx9231 <szx9231@gmail.com>
2019-08-15 22:12:39 +0800

    add carousel component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
+import Carousel from './components/carousel/Carousel';
+
+import pic1 from './components/carousel/images/pic1.jpg';
+import pic2 from './components/carousel/images/pic2.jpg';
+import pic3 from './components/carousel/images/pic3.jpg';
+
 import './App.css';
 class App extends Component {
   render() {
@@ -13,6 +19,7 @@ class App extends Component {
       <div>
         <Header blogInfo={blogInfo}></Header>
         <Nav />
+        <Carousel imgUrls={[pic1, pic2, pic3]}/>
       </div>
     );
   }
---
+++ src/components/content/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Content extends React.Component{
+    render(){
+        return ();
+    }
+}
+
+export default Content;
