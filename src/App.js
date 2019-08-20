371088abc454ff5097bc1acee91b3131cf75b682
John Doe <jdoe@email.com>
2019-08-20 17:48:45 +0800

    add post component style

--- src/App.js
+++ src/App.js
@@ -10,12 +10,32 @@ import pic2 from './components/carousel/images/pic2.jpg';
 import pic3 from './components/carousel/images/pic3.jpg';
 
 import './App.css';
+
+const PostList = [
+  { id: '1', postMediaUrl: pic1, postTitle: 'title1', postDate: '2019年1月20日', postBrief: 'test1111', postUrl: '/' },
+  { id: '2', postMediaUrl: pic2, postTitle: 'title2', postDate: '2019年2月20日', postBrief: 'test2222', postUrl: '/' },
+  { id: '3', postMediaUrl: pic3, postTitle: 'title3', postDate: '2019年12月20日', postBrief: 'test3333', postUrl: '/' },
+  { id: '4', postMediaUrl: pic1, postTitle: 'title4', postDate: '2019年11月20日', postBrief: 'test4444', postUrl: '/' },
+];
+
+
 class App extends Component {
   render() {
     const blogInfo = {
       blogTitle: 'szx9231',
       blogBranding: 'Way to Fun'
     };
+
+    let postList = PostList.map(item => {
+      return (
+        <Post
+          key={item.id}
+          postMediaUrl={item.postMediaUrl}
+          postTitle={item.postTitle}
+          postDate={item.postDate}
+          postBrief={item.postBrief}
+          postUrl={item.postUrl} />);
+    });
 
     return (
       <div>
@@ -25,7 +45,7 @@ class App extends Component {
           <div className="row">
             <div className="col-md-8 main-content">
               <Carousel className="carousel" urlList={[{ id: 1, path: pic1 }, { id: 2, path: pic2 }, { id: 3, path: pic3 }]} />
-              <Post postMediaUrl={pic1}></Post>
+              {postList}
             </div>
 
             <div className="col-md-4 siderbar">
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -6,36 +6,57 @@ import Button from '../button';
 
 import styles from './Post.module.css';
 
+
+const PostMeida = ({ postMediaUrl, loading, loadFailed }) => {
+  return (
+    <div className={styles.postMedia}>
+      <a href="/">
+        <Img
+          src={postMediaUrl}
+          Loading={loading}
+          LoadFailed={loadFailed}>
+        </Img>
+      </a>
+    </div>
+  );
+}
+
+
+const PostContent = ({ postTitle, postDate, postBrief }) => {
+  return (
+    <div className={styles.postContent}>
+      <h1 className={styles.title}>
+        <a href="/">{postTitle}</a>
+      </h1>
+      <time className={styles.date}>{postDate}</time>
+      <p className={styles.brief}>{postBrief}</p>
+    </div>
+  );
+}
+
 class Post extends React.Component {
 
   render() {
-    const { postMediaUrl } = this.props;
+    const { postMediaUrl, postTitle, postDate, postBrief, postUrl } = this.props;
     return (
       <div className={styles.postContainer}>
         <div className={styles.post}>
+          <PostMeida
+            postMediaUrl={postMediaUrl}
+            loading={Loading}
+            loadFailed={Loading} />
 
-          <div className={styles.postMedia}>
-            <a href="/">
-              <Img
-                src={postMediaUrl}
-                Loading={Loading}
-                LoadFailed={Loading}>
-              </Img>
-            </a>
-          </div>
-
-          <div className={styles.postContent}>
-            <h1 className={styles.title}>
-              <a href="/">史上最全的Webstorm快捷键及技巧</a>
-            </h1>
-            <time className={styles.date}>2019年8月19日</time>
-            <p className={styles.brief}>Webstorm是一款前端开发神器，此文介绍webstorm的快捷键和技巧分享。 所谓工欲善其事，必先利其器嘛，想着如果要提高开发效率，那掌握一些常用的快捷键应该是少不了的，所以把webstorm快捷键及技巧分享给大家。持续更新中</p>
-          </div>
-
+          <PostContent
+            postTitle={postTitle}
+            postDate={postDate}
+            postBrief={postBrief}
+          />
         </div >
-
         <div className={styles.postFooter}>
-          <Button className={styles.permalink} text="阅读全文" href="/"></Button>
+          <Button
+            className={styles.permalink}
+            text="阅读全文"
+            href={postUrl}></Button>
         </div>
 
       </div>
