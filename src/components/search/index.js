61e48375e739ea41348a64e5fe020a3e1c57adfe
szx9231 <szx9231@gmail.com>
2019-08-21 22:35:53 +0800

    add search item style

--- src/components/search/index.js
+++ src/components/search/index.js
@@ -3,6 +3,44 @@ import styles from './search.module.css';
 
 import { SearchButton } from '../button';
 
+const SEARCH_RESULT = [{
+  id: 1,
+  href: 'test1',
+  title: 'test1',
+  content: 'test1---content'
+},
+{
+  id: 2,
+  href: 'test2',
+  title: 'test2',
+  content: 'test2---content'
+},
+{
+  id: 3,
+  href: 'test3',
+  title: 'test3',
+  content: 'test3---content'
+},
+{
+  id: 4,
+  href: 'test4',
+  title: 'test4',
+  content: 'test4---content'
+}
+];
+
+
+const SearchItem = ({ href, title, content }) => {
+  return (
+    <div className={styles.searchItem}>
+      <a href={href} title={title}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.content}>{content}</div>
+      </a>
+    </div>
+  );
+}
+
 class Search extends React.Component {
   render() {
     const { placeholder, searchButtonText } = this.props;
@@ -16,7 +54,15 @@ class Search extends React.Component {
         </div>
 
         <div className={`${styles.resultWrapper}`}>
-          <div className={`${styles.searchResult}  ${styles.hide}`}></div>
+          {
+            SEARCH_RESULT.map(result => {
+              return (<SearchItem
+                key={result.id}
+                href={result.href}
+                title={result.title}
+                content={result.content} />);
+            })
+          }
         </div>
       </div>
     );
