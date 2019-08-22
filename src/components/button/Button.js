3c2ff6c1ec12a7d9339c7484bfe234256398ee2d
John Doe <jdoe@email.com>
2019-08-22 13:49:18 +0800

    add style of search buttom

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
-import Styles from './button.module.css';
+import Styles from './Button.module.css';
 
 const Button = ({ className, href, text }) =>
   <a className={`${Styles.button} ${className}`} href={href}>{text}</a>
