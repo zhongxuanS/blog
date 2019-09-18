05fa05f17610ae445bead758ff8d19ef83e0e0b6
szx9231 <jdoe@email.com>
2019-09-18 08:52:31 +0800

    add draft-editor example

--- src/components/edit-post/index.js
+++ src/components/edit-post/index.js
@@ -1,41 +1,54 @@
-import React from 'react';
-import { Editor, EditorState } from 'draft-js';
+import 'braft-editor/dist/index.css'
+import React from 'react'
+import BraftEditor from 'braft-editor'
 
-const styles = {
-  editor: {
-    border: '1px solid gray',
-    minHeight: '6em'
-  }
-};
+export default class BasicDemo extends React.Component {
 
-export default class EditPost extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { editorState: EditorState.createEmpty() };
-    this.onChange = (editorState) => this.setState({ editorState });
-    this.setEditor = (editor) => {
-      this.editor = editor;
-    };
-    this.focusEditor = () => {
-      if (this.editor) {
-        this.editor.focus();
-      }
-    };
+  state = {
+    editorState: BraftEditor.createEditorState('<p>Hello <b>World!</b></p>'), // 设置编辑器初始内容
+    outputHTML: '<p></p>'
   }
 
   componentDidMount() {
-    this.focusEditor();
+    this.isLivinig = true
+    // 3秒后更改编辑器内容
+    setTimeout(this.setEditorContentAsync, 3000)
+  }
+
+  componentWillUnmount() {
+    this.isLivinig = false
+  }
+
+  handleChange = (editorState) => {
+    this.setState({
+      editorState: editorState,
+      outputHTML: editorState.toHTML()
+    })
+  }
+
+  setEditorContentAsync = () => {
+    this.isLivinig && this.setState({
+      editorState: BraftEditor.createEditorState('<p>你好，<b>世界!</b><p>')
+    })
   }
 
   render() {
+
+    const { editorState, outputHTML } = this.state
+
     return (
-      <div style={styles.editor} onClick={this.focusEditor}>
-        <Editor
-          ref={this.setEditor}
-          editorState={this.state.editorState}
-          onChange={this.onChange}
-        />
+      <div>
+        <div className="editor-wrapper">
+          <BraftEditor
+            value={editorState}
+            onChange={this.handleChange}
+          />
+        </div>
+        <h5>输出内容</h5>
+        <div className="output-content">{outputHTML}</div>
       </div>
-    );
+    )
+
   }
+
 }
