1232032dc867f656287eca104d43f2e19b217934
szx9231 <szx9231@gmail.com>
2019-10-09 17:10:37 +0800

    change timeline item tail color

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -8,7 +8,7 @@ class TimeLine extends React.Component {
     return (
       <div className={styles.timelineWrapper}>
         <Timeline>
-          <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+          <Timeline.Item className={styles.timelineItem}>Create a services site 2015-09-01</Timeline.Item>
           <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
           <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
           <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
