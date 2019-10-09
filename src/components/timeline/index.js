9888d44f8613080e75d7138079330415542d22c0
szx9231 <szx9231@gmail.com>
2019-10-09 16:58:18 +0800

    add timeline component

--- src/components/timeline/index.js
+++ src/components/timeline/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import styles from './timeline.module.css';
+import { Timeline } from 'antd';
+
 
 class TimeLine extends React.Component {
   render() {
     return (
-      <div><h1>Timeline Coming soon!</h1></div>
+      <div className={styles.timelineWrapper}>
+        <Timeline>
+          <Timeline.Item>Create a services site 2015-09-01</Timeline.Item>
+          <Timeline.Item>Solve initial network problems 2015-09-01</Timeline.Item>
+          <Timeline.Item>Technical testing 2015-09-01</Timeline.Item>
+          <Timeline.Item>Network problems being solved 2015-09-01</Timeline.Item>
+        </Timeline>
+      </div>
     );
   }
 }
